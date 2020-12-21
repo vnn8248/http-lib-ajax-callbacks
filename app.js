01c5cf7f0da67087536cc5d761331d0e1e32159c
Bibi Westside <vnn8248@gmail.com>
2020-12-21 17:55:55 -0500

    Convert to async/await

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const http = new EasyHTTP;
 // .catch(err => console.log(err))
 
 // Create users
-// const data = {
-//     name: "Steve",
-//     user: "steveDoe",
-//     email: "sdoe@gmail.com"
-// }
+const data = {
+    name: "Steve",
+    user: "steveDoe",
+    email: "sdoe@gmail.com"
+}
 // Post user
 // http.post("https://jsonplaceholder.typicode.com/users", data)
 // .then(data => console.log(data))
@@ -18,11 +18,11 @@ const http = new EasyHTTP;
 
 
 // Update user
-// http.put("https://jsonplaceholder.typicode.com/users/10", data)
+// http.put("https://jsonplaceholder.typicode.com/users/5", data)
 // .then(data => console.log(data))
 // .catch(err => console.log(err))
 
 // Delete user
-// http.delete("https://jsonplaceholder.typicode.com/users/10")
-// .then(data => console.log(data))
-// .catch(err => console.log(err))
+http.delete("https://jsonplaceholder.typicode.com/users/10")
+.then(data => console.log(`ID:${data.id} deleted.`))
+.catch(err => console.log(err))
--- easyhttp.js
+++ easyhttp.js
@@ -1,61 +1,43 @@
 class EasyHTTP {
 
     // HTTP GET request
-    get(url) {
-        return new Promise((resolve, reject) => {
-            fetch(url)
-            .then(res => res.json())
-            .then(data => resolve(data))
-            .catch(err => reject(err))
-        })
-
+    async get(url) {
+        const response = await fetch(url);
+        const resData = await response.json();
+        return resData;
     }
 
     // HTTP POST request
-    post(url, data) {
-        return new Promise((resolve, reject) => {
-            fetch(url, {
-                method: "POST",
-                headers: {
-                    "Content-type": "application/json"
-                },
-                body: JSON.stringify(data)
-            })
-            .then(res => res.json())
-            .then(data => resolve(data))
-            .catch(err => reject(err))
+    async post(url, data) {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(data)
         })
+        const resData = await response.json();
+        return resData;
     }
 
     // HTTP PUT request
-    put(url, data) {
-        return new Promise((resolve, reject) => {
-            fetch(url, {
-                method: "PUT",
-                headers: {
-                    "Content-type": "application/json"
-                },
-                body: JSON.stringify(data)
-            })
-            .then(res => res.json())
-            .then(data => resolve(data))
-            .catch(err => reject(err))
+    async put(url, data) {
+        const response = await fetch(url, {
+            method: "PUT",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(data)
         })
+        const resData = await response.json();
+        return resData;
     }
 
     // HTTP Delete request
-    delete(url) {
-        return new Promise((resolve, reject) => {
-            fetch(url, {
-                method: "DELETE",
-                headers: {
-                    "Content-type": "application/json"
-                }
-            })
-            .then(res => res.json())
-            .then(data => resolve("Resource deleted."))
-            .catch(err => reject(err))
-        })
+    async delete(url) {
+        const response = await fetch(url);
+        const resData = await response.json();
+        return resData;
     }
 }
 
